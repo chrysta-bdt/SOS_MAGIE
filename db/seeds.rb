4aa58a2a73dce1ae118feb4230c91e5e5d935784
Subrina Joseph <subrinajoseph@hotmail.com>
2022-03-18 15:17:19 -0400

    Service form updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Service.destroy_all
 User.destroy_all
@@ -32,14 +25,14 @@ service2 = Service.create!(name: "Préparation de potion d'amour", category: 'lo
                           description: "Cette potion touche à toutes les facettes de l’amour : la douceur des sentiments à travers ses arômes fruités de cerises et le côté piquant de la passion avec ses pincées de piment qui viendront réchauffer doucement les papilles et le coeur de votre bien aimé(e). (1 flacon de 100 ml).",
                           price: 75, witch: user2, image_url: "https://images.immediate.co.uk/production/volatile/sites/7/2013/02/GettyImages-503196822_Shy-714baa1.jpg?quality=90&resize=620,413")
 service9 = Service.create!(name: 'Augmentation de libido', category: 'love',
-                          description: "Notre duo de rituel et concoction magique vous garantie de résoudre tous vos soucis lié à votre sexualité (troubles de sexualité, l'impuissance sexuelle, la frigidité ou l'éjaculation précoce)(Efficacité: instantané)",
+                          description: "Notre duo de rituel et concoction magique vous garantie de résoudre tous vos soucis liés à votre sexualité (troubles de sexualité, l'impuissance sexuelle, la frigidité ou l'éjaculation précoce)(Efficacité: instantané)",
                           price: 150, witch: user9, image_url: "https://cdn.shopify.com/s/files/1/0012/2522/8355/products/Magic_Potion_2_2048x.jpg?v=1539191648")
 service10 = Service.create!(name: "Mariage spirituel", category: 'love',
-                          description: "Officialisé par HARRY POTTER, ce rituel de mariage comblera les couples voulant s'unir aux forces de la nature pendant leur cérémonie enchanté..",
+                          description: "Officialisé par HARRY POTTER, ce rituel de mariage comblera les couples voulant s'unir aux forces de la nature pendant leur cérémonie enchantée..",
                           price: 1000, witch: user10, image_url: "https://www.hazlewood-castle.co.uk/storage//uploads/variants/119/wed29-1.jpg")
 
 service3 = Service.create!(name: 'Lecture de boule de cristal', category: 'theft',
-                          description: "Vous connaitrez l'identité de celui qui vous vol (une séance de 30 minutes).",
+                          description: "Vous connaitrez l'identité de celui qui vous vole (une séance de 30 minutes).",
                           price: 150, witch: user3, image_url: "https://images.unsplash.com/photo-1551029506-0807df4e2031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1034&q=80")
 
 service4 = Service.create!(name: "Jeter un mauvais sort (malédiction)", category: 'theft',
@@ -47,26 +40,20 @@ service4 = Service.create!(name: "Jeter un mauvais sort (malédiction)", categor
                           price: 250, witch: user4, image_url: "https://i.ytimg.com/vi/itH5xyVqMDE/maxresdefault.jpg")
 
 service5 = Service.create!(name: 'Création de poupée', category: 'conflict',
-                          description: 'Prenez le control de ta cible et arrivez à votre fin.(efficacité en moins de 24 heures) ',
+                          description: 'Prenez le controle de votre cible et arrivez à votre fin. (efficacité en moins de 24 heures) ',
                           price: 300, witch: user5, image_url: "https://www.creativeboom.com/uploads/articles/68/68c108d28851f4d15db365810fdefc15c769d35c_810.jpeg")
 
 service6 = Service.create!(name: 'Livre de recettes magiques', category: 'conflict',
-                          description: 'Un livre de recettes magiques qui permettre de calmer tous types de conflits(mariage, travail, famille...). ',
+                          description: 'Un livre de recettes magiques qui permettra de calmer tous types de conflits (mariage, travail, famille...) (10 recettes). ',
                           price: 200, witch: user6, image_url: "https://cdn.myonlinestore.eu/930d50f2-fed9-46b9-9d1e-ce811954cdc8/image/cache/full/e418afc142d0a25970a3f646544265caf87c5d96.jpg")
 
 service7 = Service.create!(name: 'Manipulation de Pendule', category: 'money',
-                          description: 'Découvrez comment réussir votre vie grace à notre pendule âgé deplus de 500 ans. (une séance de 30 minutes)',
+                          description: 'Découvrez comment réussir votre vie grâce à notre pendule âgé de plus de 500 ans. (une séance de 30 minutes)',
                           price: 100, witch: user7, image_url: "https://images.unsplash.com/photo-1616785550406-0fd04cac3c24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80)")
 
 service8 = Service.create!(name: "Rituel pour attirer l'argent", category: 'money',
-                          description: "Un rituel qui consiste d'incantations puissantes qui vous permettra d'être propriétaire d'un arbre d'argent (Efficacité dans une semaine)",
+                          description: "Un rituel qui consiste en des incantations puissantes qui vous permettra d'être propriétaire d'un arbre d'argent (Efficacité dans 3 jours)",
                           price: 500, witch: user8, image_url: "https://avaazdo.s3.amazonaws.com/f09905871aa2cba81b04ec72d0cbae6d.jpg")
 
-# service5 = Service.create(name: '', category: 'theft', description: '', price: '')
-# service6 = Service.create(name: '', category: 'conflict', description: '', price: '')
-# service7 = Service.create(name: '', category: 'money', description: '', price: '')
-# service8 = Service.create(name: , category: 'money', description: '', price: '')
-# service9 = Service.create(name: '', category: 'love', description: '', price: '')
-# service10 = Service.create(name: '', category: 'theft', description: '', price: '')
 
 puts "Finished creating seeds!"
